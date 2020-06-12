a95a237e9f8dd8d8e6e52779b6a1d85de96d6d60
andreyvarlamov <varlamov.and@gmail.com>
2020-06-11 18:34:30 -0700

    Implement simple word splitting and display

--- client/src/components/elements/PushkinEditor.js
+++ client/src/components/elements/PushkinEditor.js
@@ -1,10 +1,29 @@
-import React from "react";
-import { Container, Input } from "@material-ui/core";
+import React, { useState, useReducer, useEffect } from "react";
+import { Container, Input, Typography } from "@material-ui/core";
 
 import useStyles from "./PushkinEditorStyles";
 
 function PushkinEditor() {
   const classes = useStyles();
+
+  const wordsReducer = (state, action) => {
+    switch (action.type) {
+      case "UPDATE_WORDS":
+        return {
+          ...state,
+          split: [...new Set(action.payload.split(" "))],
+        };
+      default:
+        return state;
+    }
+  };
+
+  const [words, wordsDispatch] = useReducer(wordsReducer, { split: [] });
+  const [inputText, setInputText] = useState("");
+
+  useEffect(() => {
+    wordsDispatch({ type: "UPDATE_WORDS", payload: inputText });
+  }, [inputText]);
 
   return (
     <div className={classes.editorContainerWrapper}>
@@ -18,10 +37,16 @@ function PushkinEditor() {
             disableUnderline
             className={classes.textInput}
             classes={{ input: classes.textInputField }}
+            onChange={e => setInputText(e.target.value)}
+            value={inputText}
           />
         </div>
         <div className={classes._placeholderDiv}>
-          <p>Test</p>
+          {words.split.map((word, index) => (
+            <Typography variant="body2" key={index}>
+              {word}
+            </Typography>
+          ))}
         </div>
       </Container>
     </div>
